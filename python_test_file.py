7ffa822701b08d1fa139f2cad630fb9af535c4fb
Sri Karan Reddy Pailla <47120322+karanreddy9@users.noreply.github.com>
2021-11-25 17:20:46 -0600

    Update python_test_file.py

--- python_test_file.py
+++ python_test_file.py
@@ -1,10 +1,10 @@
 # This program adds two numbers
 
-num1 = 1.5
-num2 = 6.3
+Num1 = 1.5
+num__2 = 6.3
 
 # Add two numbers
-sum = num1 + num2
+sum = Num1 + num__2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(Num1, num__2, sum))
